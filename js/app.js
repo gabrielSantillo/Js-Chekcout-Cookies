067bfae5abf027f33d2b3ed5855823e301779619
GabrielSantillo <81308836+gabrielSantillo@users.noreply.github.com>
2022-07-20 14:03:22 -0600

    Adding comments to the codes

--- js/app.js
+++ js/app.js
@@ -1,20 +1,26 @@
+/* This function set the cookie to name selection and value black and then redirect the user to the selection.html page */
 function addBlackSneaker(details) {
     Cookies.set(`selection`, `black`);
     window.location.href = `selection.html`;
 }
+/* This variable gets the button that add the black sneaker and then call the function addBlackSneaker after the button being clicked */
 let add_black_sneaker = document.getElementById(`add_black_sneaker`);
 add_black_sneaker.addEventListener(`click`, addBlackSneaker);
 
+/* This function set the cookie to name selection and value orange and then redirect the user to the selection.html page */
 function addOrangeSneaker(details) {
     Cookies.set(`selection`, `orange`);
     window.location.href = `selection.html`;
 }
+/* This variable gets the button that add the orange sneaker and then call the function addOrangeSneaker after the button being clicked */
 let add_orange_sneaker = document.getElementById(`add_orange_sneaker`);
 add_orange_sneaker.addEventListener(`click`, addOrangeSneaker);
 
+/* This function set the cookie to name selection and value blue and then redirect the user to the selection.html page */
 function addBlueSneaker(details) {
     Cookies.set(`selection`, `blue`);
     window.location.href = `selection.html`;
 }
+/* This variable gets the button that add the blue sneaker and then call the function addBlueSneaker after the button being clicked */
 let add_blue_sneaker = document.getElementById(`add_blue_sneaker`);
 add_blue_sneaker.addEventListener(`click`, addBlueSneaker);
--- js/selection.js
+++ js/selection.js
@@ -1,25 +1,32 @@
+/* This function redirect the user to the index.html */
 function backToSneakersOptions(details) {
     window.location.href = `index.html`
 }
+/* This variable gets the back button then call the function backToSneakerOption after being clicked */
 let button_back = document.getElementById(`button_back`);
 button_back.addEventListener(`click`, backToSneakersOptions);
 
-
+/* This variable gets the value chosen by the user */
 let chosen_sneaker = Cookies.get(`selection`);
 
+/* This if statment compares the value chosen by the user. If was the black shoe, will add the image on the page */
 if (chosen_sneaker === `black`) {
     document.getElementById(`button_back`).insertAdjacentHTML(`afterEnd`, `<br><img src="https://images.pexels.com/photos/6050918/pexels-photo-6050918.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" id="selection_${chosen_sneaker}">`)
 } 
+/* If was the orange shoe, will add the image on the page */
 else if (chosen_sneaker === `orange`) {
     document.getElementById(`button_back`).insertAdjacentHTML(`afterEnd`, `<br><img src="https://images.pexels.com/photos/6050922/pexels-photo-6050922.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" id="selection_${chosen_sneaker}">`)
 } 
+/* If was the blue shoe, will add the image on the page */
 else if (chosen_sneaker === `blue`) {
     document.getElementById(`button_back`).insertAdjacentHTML(`afterEnd`, `<br><img src="https://images.pexels.com/photos/6050924/pexels-photo-6050924.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" id="selection_${chosen_sneaker}">`)  
 } 
+/* And if the value is undefined, will add a message telling the user to go back to the options page */
 else {
     document.getElementById(`button_back`).insertAdjacentHTML(`afterEnd`, `<h1>Go back to the Sneakers Options and chose your sneaker.</h1>`)
 }
 
+/* This block of code style the back button on the page */
  button_back[`style`][`backgroundImage`] = `linear-gradient(45deg, #f84a4a, #f18181)`;
  button_back[`style`][`width`] = `250px`;
  button_back[`style`][`height`] = `50px`;
@@ -31,6 +38,7 @@ else {
  button_back[`style`][`justify-self`] = `center`;
  button_back[`style`][`marginLeft`] = `40vw`;
 
+ /* This block of code style the chosen option by the user on the page */
 let image = document.getElementById(`selection_${chosen_sneaker}`);
 image[`style`][`width`] = `350px`;
 image[`style`][`height`] = `350px`;
